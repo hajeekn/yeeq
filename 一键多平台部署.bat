fc72dbfa2a6fb022a68a1d92c80044c80e818580
YUTONGWU <superxzlove@qq.com>
2020-09-06 14:12:22 +0800

    Update Website

--- 一键多平台部署.bat
+++ 一键多平台部署.bat
@@ -1,14 +1,13 @@
 @echo
 title SL专属多平台部署脚本
 color 6
-title 正在进行Coding&GitHub&Zeit平台部署
+title 正在进行Coding&GitHub平台部署
 "./Hexo Deploy Tools.exe"
 title 已完成~
 color 4
-@echo off pause
-clear
+pause
 title 正在进行GitLab平台部署
 git add --all && git commit -m "Update Website" && git push -u gitlab master -f
 title 部署完成~
 color 1
-@echo off pause
+pause
