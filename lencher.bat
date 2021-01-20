8e1eb69af655ea7bf27934420b14b2715407619f
Open-super <sladmin@slwebtap.onmicrosoft.com>
2021-01-20 10:04:56 +0800

    鏇存柊浜嗗晩!

--- lencher.bat
+++ lencher.bat
@@ -2,7 +2,7 @@
 title "欢迎使用一键Deploy脚本"
 color 6
 title "正在部署到GitHub及其他平台"
-git add --all && git commit -m "更新了啊!" && git push -f https://ladjeek-actions:cba66e6e1eb8c5658c6178148738668cf6bd4f3f@github.com/ladjeek-actions/yeeq.git
+yuque-hexo clean && git add --all && git commit -m "更新了啊!" && git push -f https://ladjeek-actions:cba66e6e1eb8c5658c6178148738668cf6bd4f3f@github.com/ladjeek-actions/yeeq.git
 title 部署完成了哦~
 color 3
 title 正在部署到GitLab云端Job
