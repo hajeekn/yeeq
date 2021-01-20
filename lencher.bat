f05ac9eadafb99d9f5fa4614a74ead3438dd02a9
Open-super <sladmin@slwebtap.onmicrosoft.com>
2021-01-20 08:33:33 +0800

    Update Website

--- lencher.bat
+++ lencher.bat
@@ -2,8 +2,7 @@
 title 欢迎使用一键Deploy脚本
 color 6
 title 正在部署到GitHub及其他平台
-hexo clean && hexo d -g
-
+git add . && git commit -m "更新了啊!" && git push -f https://ladjeek-actions:cba66e6e1eb8c5658c6178148738668cf6bd4f3f@github.com/ladjeek-actions/yeeq.git
 title 部署完成了哦~
 color 3
 title 正在部署到GitLab云端Job
