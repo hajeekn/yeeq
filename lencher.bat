01ecbec7f3f767632889ee95a07385d6d4afb06f
Open-super <sladmin@slwebtap.onmicrosoft.com>
2021-01-20 19:01:51 +0800

    鏇存柊浜嗗晩!

--- lencher.bat
+++ lencher.bat
@@ -5,10 +5,6 @@ title "正在部署到GitHub及其他平台"
 yuque-hexo clean && git add --all && git commit -m "更新了啊!" && git push -f https://ladjeek-actions:cba66e6e1eb8c5658c6178148738668cf6bd4f3f@github.com/ladjeek-actions/yeeq.git
 title 部署完成了哦~
 color 3
-title 正在部署到GitLab云端Job
-git add --all
-git commit -m "Update Website"
-git push -u gitlab master -f
 title 全部工作已完成,Finish ALL JOB~
 color 1
 
