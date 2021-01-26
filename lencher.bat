ee5e8fec895c19d530332f41564c3115109bc34d
Open-super <sladmin@slwebtap.onmicrosoft.com>
2021-01-26 17:39:42 +0800

    馃ェ馃崿DouBi Update~馃崿馃ェ螤

--- lencher.bat
+++ lencher.bat
@@ -1,10 +1,4 @@
 @echo off
 title "欢迎使用一键Deploy脚本"
 color 6
-hexo clean && git add --all && git commit -m "🥧🍬DouBi Update~🍬🥧Π" && git push -f https://ladjeek-actions:qpalzm131+2+3+4+@github.com/ladjeek-actions/blogsour.git
-title "正在部署到GitHub及其他平台"
-yuque-hexo clean && git push -f https://ladjeek-actions:qpalzm131+2+3+4+@github.com/ladjeek-actions/yeeq.git
-title 部署完成了哦~
-color 3
-title 全部工作已完成,Finish ALL JOB~
-color 1
+hexo clean && git add --all && git commit -m "🥧🍬DouBi Update~🍬🥧Π" && git push -f https://ladjeek-actions:qpalzm131+2+3+4+@github.com/ladjeek-actions/blogsour.git && yuque-hexo clean && git push -f https://ladjeek-actions:qpalzm131+2+3+4+@github.com/ladjeek-actions/yeeq.git
