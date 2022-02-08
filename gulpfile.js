94359f16490666d71c303a7014a6eedf0ac7268d
ladjeek-actions <admin@hesiy.cn>
2022-02-08 13:03:08 +0800

    修复

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,46 @@
-const gulp = require('gulp')
-const cleanCSS = require('gulp-clean-css')
-const htmlmin = require('gulp-html-minifier-terser')
-const htmlclean = require('gulp-htmlclean')
-const imagemin = require('gulp-imagemin')
-
-// babel
-const uglify = require('gulp-uglify');
-const babel = require('gulp-babel');
-
-//minify js babel
+//用到的各个插件
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-html-minifier-terser');
+var htmlclean = require('gulp-htmlclean');
+// gulp-tester
+var terser = require('gulp-terser');
+// 压缩js
 gulp.task('compress', () =>
-  gulp.src(['./public/**/*.js', '!./public/**/*.min.js','!./public/js/wow_init.js'])
-		.pipe(babel({
-			presets: ['@babel/preset-env']
-		}))
-    .pipe(uglify().on('error', function(e){
-      console.log(e);
-    }))
-		.pipe(gulp.dest('./public'))
-);
-
-// minify js - gulp-tester (如果使用 gulp-tester,把下面前面的//去掉)
-// gulp.task('compress', () =>
-//   gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-//     .pipe(terser())
-//     .pipe(gulp.dest('./public'))
-// )
-
-//css
-gulp.task('minify-css', () => {
-  return gulp.src('./public/**/*.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie11'
-    }))
-    .pipe(gulp.dest('./public'));
-});
-
-
-// 壓縮 public 目錄內 html
-gulp.task('minify-html', () => {
-  return gulp.src(['./public/**/*.html','!./public/comments/index.html'])
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-      removeComments: true, //清除 HTML 註釋
-      collapseWhitespace: true, //壓縮 HTML
-      collapseBooleanAttributes: true, //省略布爾屬性的值 <input checked="true"/> ==> <input />
-      removeEmptyAttributes: true, //刪除所有空格作屬性值 <input id="" /> ==> <input />
-      removeScriptTypeAttributes: true, //刪除 <script> 的 type="text/javascript"
-      removeStyleLinkTypeAttributes: true, //刪除 <style> 和 <link> 的 type="text/css"
-      minifyJS: true, //壓縮頁面 JS
-      minifyCSS: true, //壓縮頁面 CSS
-      minifyURLs: true
-    }))
+  gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+    .pipe(terser())
     .pipe(gulp.dest('./public'))
+)
+//压缩css
+gulp.task('minify-css', () => {
+    return gulp.src(['./public/**/*.css'])
+        .pipe(cleanCSS({
+            compatibility: 'ie11'
+        }))
+        .pipe(gulp.dest('./public'));
 });
-
-// 壓縮 public/uploads 目錄內圖片
-gulp.task('minify-images', async () => {
-  gulp.src('./public/img/**/*.*')
-    .pipe(imagemin({
-      optimizationLevel: 5, //類型：Number  預設：3  取值範圍：0-7（優化等級）
-      progressive: true, //類型：Boolean 預設：false 無失真壓縮jpg圖片
-      interlaced: true, //類型：Boolean 預設：false 隔行掃描gif進行渲染
-      multipass: true, //類型：Boolean 預設：false 多次優化svg直到完全優化
-    }))
-    .pipe(gulp.dest('./public/img'));
+//压缩html
+gulp.task('minify-html', () => {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true, //清除html注释
+            collapseWhitespace: true, //压缩html
+            collapseBooleanAttributes: true,
+            //省略布尔属性的值，例如：<input checked="true"/> ==> <input />
+            removeEmptyAttributes: true,
+            //删除所有空格作属性值，例如：<input id="" /> ==> <input />
+            removeScriptTypeAttributes: true,
+            //删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true,
+            //删除<style>和<link>的 type="text/css"
+            minifyJS: true, //压缩页面 JS
+            minifyCSS: true, //压缩页面 CSS
+            minifyURLs: true  //压缩页面URL
+        }))
+        .pipe(gulp.dest('./public'))
 });
-
-// 執行 gulp 命令時執行的任務
-gulp.task("default", gulp.parallel(
-    'compress','minify-html', 'minify-css', 'minify-images'
-)));
+// 运行gulp命令时依次执行以下任务
+gulp.task('default', gulp.parallel(
+  'compress', 'minify-css', 'minify-html'
+))
