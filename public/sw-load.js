c83fb018836ecee5ff0523aeae48b40d43e8a593
ladjeek-actions <admin@hesiy.cn>
2022-05-15 16:12:03 +0800

    停用语雀,转为本地文章,修改配置,删除沉淀文件

--- public/sw-load.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker&&window.addEventListener("load",(async()=>{navigator.serviceWorker.register(`./sw.js?time=${(new Date).getTime()}`).then((async e=>{"true"!=window.localStorage.getItem("install-sw")&&(window.localStorage.setItem("install-sw","true"),setTimeout((()=>{window.location.search=`?time=${(new Date).getTime()}`}),1e3))})).catch((e=>{}))}));
